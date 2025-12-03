bf682dcbe3194354b0bfde349a4d0c72cafa3f2f
IEEEMOSTAFA <skmostafa8888@gmail.com>
2025-12-04 00:12:50 +0600

    server added

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,6 @@
-import express,{NextFunction, Request,Response} from "express";
-
+import app from "./app"
 import config from "./config";
-import initDB, { pool } from "./config/db";
-
-import { logger } from "./middleware/logger";
-import { useRoutes } from "./modules/user/user.routes";
-import { todoRoutes } from "./modules/todo/todo.routes";
-import { authRoutes } from "./modules/auth/auth.routes";
-const app = express();
 const port = config.port;
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-//Database function:
-
-
-
-initDB();
-
-
-// users operation:
-app.use("/users",useRoutes);
-
-// todo CRUD OPERATION::
-app.use("/todos",todoRoutes);
-
-//   AUTH ROUTES:
-
-app.use("/auth", authRoutes);
-
-
-
-
-// Not Found Route Operation:
-
-app.use((req,res) =>{
-  res.status(404).json({
-    success: false,
-    message: "Not Found....Wrong Attack",
-    path: req.path
-  })
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
